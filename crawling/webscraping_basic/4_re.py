e73cb650239912775c5b11ba009e59761a0dac0c
Han Seungseok <cream5432111@gmail.com>
2024-02-10 18:36:21 +0900

    re_package

--- crawling/webscraping_basic/4_re.py
+++ crawling/webscraping_basic/4_re.py
@@ -32,7 +32,7 @@ print(lst)
 # 3. m = p.search("비교할 문자열") : 주어진 문자열 중에 일치하는게 있는지 확인
 # 4. list = p.findall("비교할 문자열") : 일치하는 모든 것을 "리스트" 형태로 반환
 
-# 원하는 형태 : 정규ㅣㄱ
+# 원하는 형태 : 정규식
 # . (ca.e) : 하나의 문자를 의미 >  care, cafe, case (O) | caffe(X)
 # ^ (^de) : 문자열의 시작 > desk, destination (O) | fade (X)
 # $ (se$) : 문자열의 끝 > case, base (O) | face (X)
