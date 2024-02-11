c12137ef629f5834d50a104bb0d73010172d5c04
Han Seungseok <cream5432111@gmail.com>
2024-02-11 20:37:26 +0900

    add environment

--- crawling/webscraping_basic/7_bs4_webtoons.py
+++ crawling/webscraping_basic/7_bs4_webtoons.py
@@ -2,16 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
-url = "https://comic.naver.com/webtoon"
+url = "https://comic.naver.com"
 res = requests.get(url, headers=headers)
 res.raise_for_status()
+
+with open("webtoon.html", "w", encoding='utf-8') as f:
+    f.write(res.text)
 
 #print(res.text)
 
 soup = BeautifulSoup(res.text, "lxml")
+print(soup.title)
 
 print(soup.a)
-cartoons = soup.find_all("div")
+cartoons = soup.find_all("a")
 print(cartoons)
 # print(soup.title)
 # print(soup.)
