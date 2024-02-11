06c4f6c2acd8cdc4c0bf547a0afa403a1d973774
Han Seungseok <cream5432111@gmail.com>
2024-02-12 03:53:19 +0900

    coupang crawling

--- crawling/webscraping_basic/4_re.py
+++ crawling/webscraping_basic/4_re.py
@@ -11,8 +11,8 @@ def print_match(m):
     if m:
         print(m.group())
         print("m.string:", m.string) # 입력받은 문자열
-        print("m.start():", m.start) # 입력받은 문자열의 시작 index
-        print("m.end():", m.end) # 일치하는 문자열의 끝 index
+        print("m.start():", m.start()) # 입력받은 문자열의 시작 index
+        print("m.end():", m.end()) # 일치하는 문자열의 끝 index
         print("m.span():", m.span()) # 일치하는 문자열의 시작 / 끝 index
     else:
         print("매칭되지 않음")
